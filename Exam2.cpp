f3d01f8e90d49a3c57dea4377e7b72e2ebc5903a
NarutoUzumaki <71786909+Juuzoo@users.noreply.github.com>
2021-01-22 13:54:09 +0300

    Add files via upload

---
+++ Exam2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream> 
+#include <string>
+
+using namespace std;
+
+class Counter
+{
+public:
+	int a = 0;
+	int num = 0;
+
+	Counter(int a)
+	{
+		this->a = a;
+		this->num = 0;
+	}
+
+	void numbers(int a)
+	{
+		if (a > 0)
+		{
+			while (num != a)
+			{
+				num = num + 1;
+				cout << "+1=" << num << "\n";
+			};
+
+			while (a > 1)
+			{
+				if (a % 2 == 0)
+				{
+					a = (a - 2);
+				}
+				else
+				{
+					a = (a - 1);
+				};
+				cout << "-2=" << a << '\n';
+			};
+		};
+	}
+};
+
+int main()
+{
+	Counter gg(0);
+	gg.numbers(90);
+	return 0;
+}
